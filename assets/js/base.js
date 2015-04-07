9f065f251a7f85ce3f04aba4d85aa9563b3d3ab5
FelipeRomero87 <contato@feliperomero.com.br>
2015-04-07 15:30:26 -0300

    Ajustes

---
+++ assets/js/base.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+var offset = 220;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.cd-top').fadeIn(duration);
+        } else {
+            jQuery('.cd-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.cd-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+
+	      $('a[href*=#]:not([href=#])').click(function() {
+	        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	          var target = $(this.hash);
+	          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	          if (target.length) {
+	            $('html,body').animate({
+	              scrollTop: target.offset().top
+	            }, 1000);
+	            return false;
+	          }
+	        }
+	  	});
+
+});
